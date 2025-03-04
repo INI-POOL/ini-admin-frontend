569ec6aae218c922e5c536fa105ca27b45688023
cherry <1379542956@qq.com>
2025-03-04 17:30:44 +0800

    +修改审核谷歌

--- dist/assets/BarChart-DzvWa5lW.js
+++
@@ -1,2 +0,0 @@
-import{a as t}from"./index-DpfcLPuk.js";import{C as o,p as r,a as e,b as s,B as p,L as n,c as i}from"./Dashboard-08PbboE0.js";import{d as l,a as m,g as u,u as c}from"./index-B0sGV8sl.js";import"./useProjects-CTWVNpZc.js";import"./ProjectStatusBadge.vue_vue_type_script_setup_true_lang-Bw48g3br.js";const k=l({__name:"BarChart",props:{data:{},options:{}},setup(_){return o.register(r,e,s,p,n,i),(a,d)=>(m(),u(c(t),{data:a.data,options:a.options},null,8,["data","options"]))}});export{k as default};
-//# sourceMappingURL=BarChart-DzvWa5lW.js.map
---
+++ dist/assets/DoughnutChart-Cq2Jwzsu.js
@@ -0,0 +1,2 @@
+import{D as e}from"./index-DzBEzjDv.js";import{C as n,p as r,a as s,b as p,A as i,c as u}from"./Dashboard-DOnbZDqp.js";import{d as l,a as m,g as c,u as g}from"./index-CILPpmAj.js";import"./useProjects-5wzYfXBU.js";import"./ProjectStatusBadge.vue_vue_type_script_setup_true_lang-CYKmZupF.js";const k=l({__name:"DoughnutChart",props:{data:{},options:{}},setup(t){n.register(r,s,p,i,u);const a=t;return(o,d)=>(m(),c(g(e),{data:a.data,options:o.options},null,8,["data","options"]))}});export{k as default};
+//# sourceMappingURL=DoughnutChart-Cq2Jwzsu.js.map
