bb8c9e7a98cff820c5b731bf1b09b3d182e1305e
Huan Shen <1178234107@qq.com>
2025-03-26 11:22:36 +0800

    remove dist

--- dist/assets/PieChart-myXbFAZ4.js
+++
@@ -1,2 +0,0 @@
-import{P as e}from"./index-D_u-fN7I.js";import{C as r,p as s,a as p,b as n,A as i,c as l}from"./Dashboard-MaNsADNl.js";import{d as m,a as c,g as u,u as d}from"./index-az7Il58q.js";import"./useProjects-Dypjh0lC.js";import"./ProjectStatusBadge.vue_vue_type_script_setup_true_lang-D5HRHrp9.js";const k=m({__name:"PieChart",props:{data:{},options:{}},setup(a){r.register(s,p,n,i,l);const t=a;return(o,_)=>(c(),u(d(e),{data:t.data,options:o.options},null,8,["data","options"]))}});export{k as default};
-//# sourceMappingURL=PieChart-myXbFAZ4.js.map
